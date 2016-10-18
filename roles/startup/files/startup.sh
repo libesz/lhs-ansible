272d31e23248350767c440f6110647f9eb8851ba
libesz <huszty.gergo@digitaltrip.hu>
2016-10-18 20:10:20 +0200

    startup script added

---
+++ roles/startup/files/startup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+stty_orig=`stty -g`
+stty -echo
+echo -ne 'nos? '
+read PASS
+stty $stty_orig
+
+
+#NEW /home
+mdadm --assemble /dev/md1 /dev/sda2 /dev/sdb2
+
+echo $PASS | cryptsetup luksOpen /dev/md1 md1_open
+if [ $? -ne 0 ]; then
+   exit 1
+fi
+
+mount /dev/mapper/md1_open /home
+if [ $? -ne 0 ]; then
+   exit 2
+fi
+
+#NEW /mnt/data
+echo $PASS | cryptsetup luksOpen /dev/sda5 sda5_open
+if [ $? -ne 0 ]; then
+   exit 1
+fi
+mount /dev/mapper/sda5_open /mnt/data
+if [ $? -ne 0 ]; then
+   exit 3
+fi
+
+systemctl start docker.socket
+if [ $? -ne 0 ]; then
+   exit 4
+fi
+
+systemctl start docker.service
+
+if [ $? -ne 0 ]; then
+   exit 5
+fi
+
+echo OK
+exit 0
